79e99e3919989315ec39125a786443693c9b0ef4
zayn-nivexa <nivexadigital@gmail.com>
2026-08-24 09:48:24 -0700

    Update vite.config.js

--- WeatherGPT/frontend/vite.config.js
+++ WeatherGPT/frontend/vite.config.js
@@ -1,17 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/WeatherGPT-2/',
   plugins: [react()],
+
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
